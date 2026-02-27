011a8125f0465fd2a4c26e203de0b3a4cf5c0a3e
dq24jp <dq24jp@brocku.ca>
2026-02-27 08:51:35 -0500

    date strings to datetime objects

--- python/common.py
+++ python/common.py
@@ -2,7 +2,13 @@ import pandas as pd
 
 
 # all data from the csv
-data = pd.read_csv("../team01_ecommerce.csv")
+raw_data = pd.read_csv("../team01_ecommerce.csv")
+
+# clean data
+data = raw_data
+# date strings to date objects
+data['date'] = pd.to_datetime(data['date'])
 
 # rows that are missing one or more pieces of data 
 missing_rows = data[data.isna().any(axis=1)]
+
