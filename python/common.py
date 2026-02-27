500fea1368758941dd74358e6e79790eeb7dd3f3
dq24jp <dq24jp@brocku.ca>
2026-02-27 12:31:57 -0500

    formatting

--- python/common.py
+++ python/common.py
@@ -4,7 +4,9 @@ import pandas as pd
 # all data from the csv
 raw_data = pd.read_csv("../team01_ecommerce.csv")
 
-# clean data
+# clean data:
+
+
 data = raw_data
 # date strings to date objects
 data['date'] = pd.to_datetime(data['date'])
